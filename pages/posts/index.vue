a05de8286f1ae2cc914c55fdfa804c61f75648ef
moudev <rigobertomauriciomartinez@gmail.com>
2020-01-19 16:02:52 -0600

    Update index posts page to show publications list
      - Adding the request to get list posts from
        GitHub issues
      - Adding the environment variable of the repository
        that will work as backend

--- pages/posts/index.vue
+++ pages/posts/index.vue
@@ -1,13 +1,57 @@
 <template>
   <div class="container-page">
-    <div>
-      posts
+    <div class="posts-list">
+      <div v-for="post in posts" :key="post.id" class="post-card">
+        <nuxt-link :to="`/posts/${post.number}`">
+          <p class="post-card__title">
+            {{ post.post.title }}
+          </p>
+          <p class="post-card__description">
+            {{ post.post.description }}
+          </p>
+        </nuxt-link>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {}
+import _ from 'lodash'
+
+export default {
+  data() {
+    return {
+      posts: []
+    }
+  },
+  async mounted() {
+    const publications = _.values(await this.getPosts())
+    const formattedPosts = publications.map(p => ({
+      ...p,
+      post: {
+        ...JSON.parse(p.title),
+        content: p.body
+      }
+    }))
+    this.posts = formattedPosts
+  },
+  methods: {
+    getPosts() {
+      return this.$axios.$get('issues')
+    }
+  },
+  head() {
+    return {
+      title: 'Publications'
+    }
+  }
+}
 </script>
 
-<style></style>
+<style>
+.post-card {
+  border: 1px solid red;
+  margin: 15px 0px;
+  background: #ebf8ff;
+}
+</style>
