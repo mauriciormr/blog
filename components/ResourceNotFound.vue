5f5841e0deb81a215ebdd8af3ec4c14e39b7f99a
moudev <rigobertomauriciomartinez@gmail.com>
2020-01-24 23:33:03 -0600

    Add ResourceNotFound component
    Component to show a div error that will
    contain a custom error with the logic of the
    errorHandler.
    
      - Updating post details page to display an error message
        when the post doesn't exist

---
+++ components/ResourceNotFound.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="resource-not-found">
+    <h1>{{ error.statusCode }}</h1>
+    <h2>¡Oops!</h2>
+    <p>{{ error.message }}</p>
+  </div>
+</template>
+
+<script>
+import { errorHandler } from '~/utils/validate-errors'
+
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true,
+      default: () => {}
+    }
+  },
+  mounted() {
+    this.error = errorHandler(new Error(this.error.statusCode))
+  }
+}
+</script>
+<style>
+.resource-not-found {
+  background: red;
+}
+</style>
