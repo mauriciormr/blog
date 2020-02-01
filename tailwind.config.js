c0a1f1b603daaee4b667a32890580dc0a054cbf7
moudev <rigobertomauriciomartinez@gmail.com>
2020-01-31 22:36:25 -0600

    Add base colors tailwind theme
      - Adding css color variables int the main
        css file
      - Adding custom colors in the tailwind config
        file

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@
  ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
  */
 module.exports = {
-  theme: {},
+  theme: {
+    backgroundColor: theme => ({
+      base: 'var(--bg-background-base)',
+      primary: 'var(--bg-background-primary)',
+      primarySelected: 'var(--bg-background-primary--selected)',
+      secondary: 'var(--bg-background-secondary)',
+      ternary: 'var(--bg-background-ternary)'
+    }),
+    textColor: {
+      primary: 'var(--text-primary)',
+      secondary: 'var(--text-secondary)'
+    }
+  },
   variants: {},
   plugins: []
 }
